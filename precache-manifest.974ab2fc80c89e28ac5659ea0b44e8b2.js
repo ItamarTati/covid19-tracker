aa37ff6835481db43d5484dac1ca00d5472805db
Itamar Tati <itamar.tati1994@gmail.com>
2020-04-14 09:51:10 +0100

    Updates

---
+++ precache-manifest.974ab2fc80c89e28ac5659ea0b44e8b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "983d188528ba35c037451f298479abb6",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "a624b5cd716b650b7753",
+    "url": "/covid19-tracker/static/css/main.13c8a689.chunk.css"
+  },
+  {
+    "revision": "f5a12eca876873f906e8",
+    "url": "/covid19-tracker/static/js/2.9e8e7884.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19-tracker/static/js/2.9e8e7884.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a624b5cd716b650b7753",
+    "url": "/covid19-tracker/static/js/main.5f3e75ca.chunk.js"
+  },
+  {
+    "revision": "3472a32df87a999f1966",
+    "url": "/covid19-tracker/static/js/runtime-main.763ff21b.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19-tracker/static/media/image.d7265326.png"
+  }
+]);
